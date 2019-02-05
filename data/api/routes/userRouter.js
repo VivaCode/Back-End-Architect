5c85815f2dfc27d4b7723e4076f4437c9722b63e
brooks <bpoltl1@gmail.com>
2019-02-05 16:07:42 -0600

    deleted comment

--- data/api/routes/userRouter.js
+++ data/api/routes/userRouter.js
@@ -3,7 +3,7 @@ const knex = require ('knex');
 const router = express.Router ();
 const helper = require('../helpers/userHelpers')
 
-//
+
 router.get('/', (req,res)=>{
     helper.getUsers()
     .then(users =>{
